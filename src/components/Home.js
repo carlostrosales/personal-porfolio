843c7759d685093d59d6e57de4b68a4407e86e62
TheCarlosTovias <carlos.tovias22@gmail.com>
2023-06-05 22:43:07 -0500

    Creating home page and added basic text with style objects binded to component

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const homeStyle = {
+    backgroundColor: 'white'
+};
+
+const Home = () => {
+    return (
+      <div style={homeStyle}>
+        <h1>Carlos Rosales</h1>
+      </div>
+    );
+}
+
+export default Home;
+
+
