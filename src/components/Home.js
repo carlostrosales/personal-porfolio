6651f2b35197621dd1ed74ca672bbbeffce139b7
TheCarlosTovias <carlos.tovias22@gmail.com>
2023-06-07 23:20:21 -0500

    Modified style objects in component for h1 title

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,17 @@ const homeStyle = {
     backgroundColor: 'white'
 };
 
+const titleStyle = {
+    maxWidth: '100%',
+    fontSize: '350%',
+    marginTop: '5%',
+    marginBottom: '2%',
+}
+
 const Home = () => {
     return (
       <div style={homeStyle}>
-        <h1>Carlos Rosales</h1>
+        <h1 style={titleStyle}>Carlos Rosales</h1>
       </div>
     );
 }
