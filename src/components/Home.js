3aff1ca2658446aadaba67f57f1a238a6689d6f5
TheCarlosTovias <carlos.tovias22@gmail.com>
2023-06-15 23:29:16 -0500

    Imported image into home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,29 @@
 import React from "react";
+import profilePic from '../assets/profile-pic.jpeg'
 
 const homeStyle = {
-    backgroundColor: 'white'
-};
+    backgroundColor: 'white',
+}
 
 const titleStyle = {
     maxWidth: '100%',
     fontSize: '350%',
     marginTop: '5%',
     marginBottom: '2%',
+    float: 'left',
+}
+
+const imageStyle = {
+  float: 'right',
 }
 
 const Home = () => {
     return (
       <div style={homeStyle}>
         <h1 style={titleStyle}>Carlos Rosales</h1>
+        <img style={imageStyle} src={profilePic} alt="profilePic" />
       </div>
     );
 }
 
 export default Home;
-
-
