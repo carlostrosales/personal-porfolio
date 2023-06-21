f9c2f6109c6291a2317ddccd984115ab7f287661
TheCarlosTovias <carlos.tovias22@gmail.com>
2023-06-20 22:17:01 -0500

    Added typewritter effect to title and styled home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import profilePic from '../assets/profile-pic.jpeg'
+import Typewriter from "typewriter-effect";
 
 const homeStyle = {
     backgroundColor: 'white',
@@ -10,18 +11,40 @@ const titleStyle = {
     fontSize: '350%',
     marginTop: '5%',
     marginBottom: '2%',
-    float: 'left',
+    float: 'right',
+    paddingTop: '15%',
+    paddingRight: '20%',
+    fontFamily: 'nobel',
+    fontWeight: '300',
+    fontStyle: 'normal',
 }
 
 const imageStyle = {
+  float: 'left',
+  width: '500px',
+  paddingTop: '10%',
+  paddingLeft: '10%',
+}
+
+const typewriterStyle = {
+  paddingRight: '35%',
   float: 'right',
 }
 
 const Home = () => {
     return (
       <div style={homeStyle}>
-        <h1 style={titleStyle}>Carlos Rosales</h1>
+        <h1 style={titleStyle}>CARLOS ROSALES</h1>
         <img style={imageStyle} src={profilePic} alt="profilePic" />
+        <div style={typewriterStyle}>
+          <Typewriter
+            options={{
+              strings: ["hey there!"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </div>
       </div>
     );
 }
