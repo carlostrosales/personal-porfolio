fdc99490bfd4b27645089f57bdd580145270ecc3
TheCarlosTovias <carlos.tovias22@gmail.com>
2023-09-21 16:19:40 -0500

    Added imports for material UI icon in Home Section and adjusted CSS

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,51 +1,45 @@
 import React from "react";
-import profilePic from '../assets/profile-pic.jpeg'
+import profilePic from "../assets/profile-pic.jpeg";
 import Typewriter from "typewriter-effect";
-import "./Home.css"
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
-
-const titleStyle = {
-    maxWidth: '100%',
-    fontSize: '350%',
-    marginTop: '5%',
-    marginBottom: '2%',
-    float: 'right',
-    paddingTop: '15%',
-    paddingRight: '20%',
-    fontFamily: 'nobel',
-    fontWeight: '300',
-    fontStyle: 'normal',
+const mainContainer = {
+  display: "flex",
+  flexDirection: "row",
+  padding: "5%",
 } as React.CSSProperties;
 
-const imageStyle = {
-  float: 'left',
-  width: '500px',
-  paddingTop: '10%',
-  paddingLeft: '10%',
-} as React.CSSProperties;
-
-const typewriterStyle = {
-  paddingRight: '35%',
-  float: 'right',
+const textContainer = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  paddingTop: "15%",
+  marginLeft: "5%",
 } as React.CSSProperties;
 
 const Home = () => {
-    return (
-      <div>
-        <h1 style={titleStyle}>CARLOS ROSALES</h1>
-        <img style={imageStyle} src={profilePic} alt="profilePic" />
-        <div style={typewriterStyle}>
+  return (
+    <div>
+      <div style={mainContainer}>
+        <img src={profilePic} alt="profilePic" />
+        <div style={textContainer}>
+          <h1>CARLOS ROSALES</h1>
           <Typewriter
             options={{
-              strings: ["hey there!"],
+              strings: [
+                "Hey there, welcome!",
+                "My name is Carlos",
+                "I'm a Software Developer",
+              ],
               autoStart: true,
               loop: true,
             }}
           />
         </div>
-        <a className="arrow" href='!#'> </a>
       </div>
-    );
-}
+      <KeyboardArrowDownIcon sx={{marginTop: -10, fontSize: 65}}/>
+    </div>
+  );
+};
 
 export default Home;
